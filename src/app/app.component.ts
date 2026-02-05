d87f93128604b375bba36f67a3661076d8e3b97a
Léa <lealeh.abnpro@gmail.com>
2026-02-05 15:20:03 +0100

    Eclaircissement du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Header1Component} from './layouts/header/header1.component';
-import {FooterComponent} from './layouts/footer/footer.component';
+import { Header1Component } from './layouts/header/header1.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header1Component, FooterComponent],
+  imports: [
+    RouterOutlet,
+    Header1Component,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'front-angular';
+  title = 'BookHub';
 }
