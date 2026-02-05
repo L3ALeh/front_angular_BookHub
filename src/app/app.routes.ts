97a2069ad61761cf5d5147be9b7260466178c168
Léa <lealeh.abnpro@gmail.com>
2026-02-05 15:21:39 +0100

    Ajout route du catalogue + commentaires

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Futur Ajout de commentaire pour la page d'accueil/dashboard + login
+
+  // route du catalogue
+   { path: 'catalogue', component: CatalogueComponent },
+];
