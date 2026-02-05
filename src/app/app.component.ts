8c621a8dcc00d1b0ad06d4a10586b9088dab1a65
CelineWau <celine.waucheul2024@campus-eni.fr>
2026-02-05 11:33:29 +0100

    Création du header et du footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header1Component} from './layouts/header/header1.component';
+import {FooterComponent} from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header1Component, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layouts/header/header1.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
+  templateUrl: './header1.component.html',
+  styleUrl: './header1.component.css'
+})
+export class Header1Component {
+  constructor(private router: Router) {}
+
+  logout() {
+    this.router.navigate(['/login']);
+    //TODO: a adapter
+  }
+}
