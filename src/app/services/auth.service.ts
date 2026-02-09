e81380652d8b43889732218a8044172507382ca4
Léa <lealeh.abnpro@gmail.com>
2026-02-09 15:35:13 +0100

    RAS

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,19 +7,19 @@ export class AuthService {
   // Scenario de test : Changement de user pour tester les deux profils
 
   // PROFIL 1 :Lecteur simple avec un emprunt
- private utilisateurConnecte = {
+ /* private utilisateurConnecte = {
     id: '83E529D7-D4AF-4C17-B35E-A63D67D02478', // UUID
     nom: 'JeanLecteur',
     roles: ['USER']
-  };
+  }; */
 
 
   // PROFIL 2 : Bibliothécaire
-  /*private utilisateurConnecte = {
+  private utilisateurConnecte = {
     id: 'E6002D4B-2470-4BB8-B4B3-52FE67629F68', // UUID de Marie
     nom: 'MarieBiblio',
    roles: ['LIBRARIAN']
-  };*/
+  };
 
   constructor() {}
 
