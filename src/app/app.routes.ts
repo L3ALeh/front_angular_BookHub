bcef0b9490fb12b194ca9166d042462f739fb277
Léa <lealeh.abnpro@gmail.com>
2026-02-10 16:25:20 +0100

    Add Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import {livreDetailsComponent} from './detailsLivre/livreDetails.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  // Futur Ajout de commentaire pour la page d'accueil/dashboard + login
-
   // route du catalogue
-   { path: 'catalogue', component: CatalogueComponent },
+   { path: 'books', component: CatalogueComponent },
   //route détails du livre
   { path: 'books/:id', component: livreDetailsComponent },
-  { path: '', redirectTo: '/catalogue', pathMatch: 'full' }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  //route dashboard
+  { path: 'dashboard', component: DashboardComponent },
 ];
